c3a42f31fe84ae463c6d384fbc9d7042d5ffb7da
mlsmoorman <melissa.s.moorman@gmail.com>
2026-05-09 11:44:03 -0500

    registers models in judging  app - admin

--- judging/admin.py
+++ judging/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import JudgeScoreSheet, KCTEntry
 
-# Register your models here.
+#####  Registering Models  #####
+@admin.register(JudgeScoreSheet)
+class JudgeScoreSheetAdmin(admin.ModelAdmin):
+    list_display = (
+		"team_entry",
+		"judge",
+		"division",
+		"total",
+		"rank",
+	)
+    list_filter = ("division", "judge", "team_entry__meet")
+    search_fields = ("team_entry__school__name", "judge__username")
+    readonly_fields = ("subtotal", "total")
+    
+@admin.register(KCTEntry)
+class KCTEntryAdmin(admin.ModelAdmin):
+    list_display = (
+		"team_entry",
+		"kct",
+		"routine_time_seconds",
+		"kick_count",
+		"falls_observed",
+		"dangerous_move_observed",
+	)
+    list_filter = ("team_entry__meet", "kct")
+    search_fields = ("team_entry__school__name",)
+    
+    
