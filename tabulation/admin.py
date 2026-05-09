31e17c93943fed20c056da9e44c8b661f1d393f5
mlsmoorman <melissa.s.moorman@gmail.com>
2026-05-09 11:47:02 -0500

    registers models in tabulation app - admin

--- tabulation/admin.py
+++ tabulation/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import TeamResult
 
-# Register your models here.
+#####  Registering Models  #####
+@admin.register(TeamResult)
+class TeamResultAdmin(admin.ModelAdmin):
+    list_display = (
+		"team_entry",
+		"rank_system",
+		"rank_total",
+		"final_place",
+		"disqualified",
+	)
+    list_filter = ("rank_system", "disqualified")
+    search_fields = ("team_entry__school__name",)
+    
