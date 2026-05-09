be4993ecbd48965c24a21b6d7d922980dba3c8e8
mlsmoorman <melissa.s.moorman@gmail.com>
2026-05-09 10:22:23 -0500

    adds meets models - Meets, Divisions, Teams, Entries

--- meets/models.py
+++ meets/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from core.models import School, User
 
-# Create your models here.
+
+##### DIVISION MODEL #####
+class Division(models.TextChoices):
+    JAZZ = "JAZZ", "Jazz"
+    KICK = "KICK", "Kick"
+    
+
+##### CLASS LEVEL MODEL #####
+class ClassLevel(models.TextChoices):
+    A = "A", "A"
+    AA = "AA", "AA"
+    AAA = "AAA", "AAA",
+    CONF = "CONF", "Conference"
+
+
+##### MEET MODEL #####
+class Meet(models.Model):
+    name = models.CharField(max_length=255)
+    date = models.DateField()
+    site = models.CharField(max_length=255)
+    class_level = models.CharField(max_length=10, choices=ClassLevel.choices)
+    division = models.CharField(max_length=10, choices=Division.choices)
+    
+    judges = models.ManyToManyField(User, related_name="judged_meets")
+    kcts = models.ManyToManyField(User, related_name="kct_meets")
+    
+    def __str__(self):
+        return f"{self.name} ({self.date})"
+
+##### TEAM MODEL #####
+class TeamEntry(models.Model):
+    meet = models.ForeignKey(Meet, on_delete=models.CASCADE, related_name="teams")
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    performance_order = models.PositiveBigIntegerField()
+    num_competitors = models.PositiveBigIntegerField()
+    
+    def __str__(self):
+        return f"{self.school} @ ({self.meet})"
