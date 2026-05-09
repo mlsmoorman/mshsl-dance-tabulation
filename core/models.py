74c2d42f5f110ce2fcc9a6b3e6f9857a2cb6a99d
mlsmoorman <melissa.s.moorman@gmail.com>
2026-05-09 10:07:00 -0500

    adds core models for users, schools, roles

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+#####  USER MODEL - SELECTS USER WHICH DETERMINES VIEW  #####
+class User(AbstractUser):
+    ROLE_CHOICES = [
+		("JUDGE", "Judge"),
+		("KCT", "Kick Counter/Timer"),
+		("TABULATOR", "Tabulator"),
+		("COACH", "Coach"),
+		("ADMIN", "Admin"),
+	]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+#####  SCHOOL MODEL - SELECTS SCHOOL  #####
+class School(models.Model):
+    name = models.CharField(max_length=255)
+    abbreviation = models.CharField(max_length=20, blank=True)
+    
+    def __str__(self):
+        return self.name
+
