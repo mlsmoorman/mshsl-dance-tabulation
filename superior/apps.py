e97f27f7fdfd0c2908269abfc355a3be0ad189d3
mlsmoorman <melissa.s.moorman@gmail.com>
2026-05-12 21:44:14 -0500

    removes superior app as this makes more sense to maintain within the judging app

--- superior/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SuperiorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'superior'
--- superior/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import superior_judge_review
-
-urlpatterns = [
-    path("<int:team_entry_id>/", superior_judge_review, name="superior_judge_review"),
-]
